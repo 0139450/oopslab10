8af47358fa465527156d2b48c2def6888abe5e84
0139450 <159297417+0139450@users.noreply.github.com>
2024-04-29 21:24:48 +0530

    Create threaddemo2.java

---
+++ threaddemo2.java
@@ -0,0 +1,33 @@
+public class threaddemo2 implements Runnable {
+    String name;
+    threaddemo2(String nam){
+        name=nam;
+    }
+    public threaddemo2() {
+        //TODO Auto-generated constructor stub
+    }
+    public void run(){System.out.println("thread run method started by "+name);}
+    public void start(){
+        System.out.println("thread execution started");
+    }
+    
+    //public void notify(){}
+    public static void main(String args[]){
+        threaddemo2 obj=new threaddemo2();
+        Thread t1=new Thread(obj);
+        Thread t2=new Thread("yamu");
+        t1.run();
+        
+        t2.run();
+        t1.start();
+        
+        t2.start();
+    }
+        
+    }
+    
+
+    
+
+
+	
